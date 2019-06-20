45a196e85da8133ca03c908320170276c7ce01b8
Andrey Safonov <safonov.andrei89@gmail.com>
2019-06-20 15:02:11 +0300

    Form button text according the purpose;

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -4,7 +4,9 @@ import './Form.css'
 const Form = ({ enteredSequence, onProcessSequence, enteredSequenceChange, historyItemIndex }) => (
   <div>
     <input type="text" onChange={enteredSequenceChange} value={enteredSequence} />
-    <button onClick={() => onProcessSequence(enteredSequence, historyItemIndex)} disabled={!enteredSequence}>Get unique sequence</button>
+    <button onClick={() => onProcessSequence(enteredSequence, historyItemIndex)} disabled={!enteredSequence}>
+      { historyItemIndex !== null ? 'Edit the sequence': 'Get unique sequence'}
+      </button>
   </div>
   );
 
